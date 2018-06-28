d0e3564eb8b11b85647d6d83066a83cb7057ae9c
Forestf <michalsliwa96@wp.pl>
2018-06-28 20:05:47 +0200

    v1.0

--- src/puzle/part.java
+++ src/puzle/part.java
@@ -13,6 +13,11 @@ import javax.swing.JPanel;
 
 import java.util.Random;
 
+
+import java.awt.Dimension;
+
+import javax.swing.JOptionPane;
+
 public class part extends JPanel {
 
 	private BufferedImage zdj;
@@ -21,10 +26,16 @@ public class part extends JPanel {
 	private Rectangle[] sektor = new Rectangle[9];
 	
 	private int[] poczatek = new int[9]; 
+	private int[] poczatek_id = new int[9];
 	
 	private int wys;
 	private int szer;
 	
+	private int ruchy=0;
+	private boolean victory=false;
+	public Graphics2D calosc;
+	
+	public int startowy;
 	public part() {
 
 		
@@ -58,6 +69,13 @@ public class part extends JPanel {
 
 			
 			if(i!=8)kawalek[i] =zdj.getSubimage(x, y, szer/3, wys/3);
+			else {
+				kawalek[i] = new BufferedImage(szer/3, wys/3, BufferedImage.TYPE_INT_ARGB);
+				Graphics2D    graphics = kawalek[i].createGraphics();
+
+				graphics.setPaint ( Color.WHITE) ;
+				graphics.fillRect ( 0, 0, kawalek[i].getWidth(), kawalek[i].getHeight() );
+			}
 			
 			sektor[i]= new Rectangle(x,y, szer/3 , wys/3);
 			
@@ -75,13 +93,14 @@ public class part extends JPanel {
 	{
 		for(int i=0 ; i<poczatek.length ; i++) {
 			poczatek[i]= i;
+			poczatek_id[i]=i;
 		}
 		Random r = new Random();
 		int losowy;
 		int temp;
 		for(int i=0 ; i<poczatek.length ; i++) {
 			
-			losowy = r.nextInt(7)+0;
+			losowy = r.nextInt(8)+0;
 			
 			temp = poczatek[i];
 			poczatek[i]=poczatek[losowy];
@@ -90,21 +109,116 @@ public class part extends JPanel {
 			
 		}
 		
-		
+		for(int i=0 ; i<poczatek.length ;i++) {
+			if(poczatek[i]==8) {
+				startowy=i;
+				break;
+			}
+		}
+		startowy =8;
 	}
+	
+
 	
 	
 	public void paint(Graphics g) {
-		Graphics2D calosc =(Graphics2D) g;
+		calosc =(Graphics2D) g;
 		int x=0 ,y=0;
+		Color c= new Color(70,130,180);
+		
+		((Graphics2D) g).setStroke(new BasicStroke(5));
+		g.setColor(c);
+		
 		
 		for(int i=0 ; i<kawalek.length ; i++) {
-			//calosc.drawImage(kawalek[i] ,x,y,this);
-			//sektor[i]= new Rectangle(x,y, szer/3 , wys/3);
+
 			calosc.drawImage(kawalek[i] , (int)sektor[poczatek[i]].getMinX(),(int)sektor[poczatek[i]].getMinY(),
 					(int)sektor[poczatek[i]].getWidth(), (int)sektor[poczatek[i]].getHeight() , null);
 		}
+		g.drawLine(szer/3, 0, szer/3, wys);
+		g.drawLine(2*szer/3, 0, 2*szer/3, wys);
+		g.drawLine(0, wys/3, szer, wys/3);
+		g.drawLine(0, 2*szer/3, szer, 2*wys/3);
+		
+		if(victory) calosc.drawImage(zdj ,x,y,this);
 		
 	}
+	
+	public void przesuniecie(int kierunek) {	
+		int docelowy=0;
+
+		int startID= poczatek[8];
+		switch(kierunek) {
+		case 0: //dol - czyli gora
+			if(startID>5) return;
+			else docelowy =(startID+3);
+			break;
+		case 1: //lewo
+			if(startID%3==0) return;
+			else docelowy = (startID-1);
+			break;
+			
+		case 2: //gora - czyli dol
+			if(startID<3) return;
+			else docelowy = (startID -3);
+			break;
+		case 3: //prawo
+			if((startID+1)%3==0) return;
+			else docelowy = (startID+1);
+			break;
+		}
+		
+	
+			int jprd=0;
+			for(int i=0 ; i<poczatek.length ; i++) {
+				if(poczatek[i]==docelowy) jprd=i;
+			}
+				
+			int temp = poczatek[8];
+			poczatek[8]=poczatek[jprd];
+			poczatek[jprd] = temp;
+			
+		
+		startowy=8;
+		ruchy++;
+		zwyciestwo();
+	}
+	
+	public void hack() {
+		for(int i=0 ; i< poczatek.length ; i++) {
+			poczatek[i]=i;
+		}
+			
+		
+	}
+	
+	public void zwyciestwo() {
+		for(int i=0 ; i< poczatek.length ; i++) {
+			if(poczatek[i]!=i)return;
+		}
+		
+		repaint();
+		JOptionPane.showMessageDialog(null, "Udalo Ci sie ulozyc puzle w "+ruchy+" ruchach", "Puzle ulozone", JOptionPane.INFORMATION_MESSAGE);
+		victory= true;
+		repaint();
+		
+		
+		int response = JOptionPane.showConfirmDialog(null, "Zaczac od poczatku ?", "Jeszcze raz ?",
+                JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+    if (response == JOptionPane.NO_OPTION) {
+            System.exit(0);
+    } 
+        else if (response == JOptionPane.YES_OPTION) {
+        	ruchy=0;
+        	victory= false;
+        	tasuj();
+    } 
+        else if (response == JOptionPane.CLOSED_OPTION) {
+            System.exit(0);
+    }
+		
+	}
+	
+
 	
 }
--- src/puzle/plansza.java
+++ src/puzle/plansza.java
@@ -1,6 +1,7 @@
 package puzle;
 
 import java.awt.*;
+import java.awt.event.KeyEvent;
 import java.io.IOException;
 import javax.swing.JFrame;
 import java.awt.image.BufferedImage;
@@ -9,24 +10,41 @@ import java.io.File;
 
 import javax.swing.JPanel;
 
-public class plansza extends JFrame{
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-	
+public class plansza extends JFrame implements KeyListener{
+
+	public JPanel obraz = new part();
 	public plansza() {
 		super("Puzle");
 		
 		
-		JPanel obraz= new part();
+		//obraz= new part();
 		add(obraz);
-		
+		addKeyListener(this);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setSize(350 , 350);
-		//pack();
+		//setSize(350 , 350);
+		pack();
 		setVisible(true);
 		
 		
 	}
 	
-
+	public void keyReleased(KeyEvent evt) {
+		int c=evt.getKeyCode();
+		if(c ==KeyEvent.VK_RIGHT)   ((part) obraz).przesuniecie(3);
+		if(c ==KeyEvent.VK_UP)   ((part) obraz).przesuniecie(2);
+		if(c ==KeyEvent.VK_LEFT)   ((part) obraz).przesuniecie(1);
+		if(c ==KeyEvent.VK_DOWN)   ((part) obraz).przesuniecie(0);
+		if(c ==KeyEvent.VK_SPACE) ((part) obraz).hack();
+		obraz.repaint();
+	}
+	
+	public void keyPressed(KeyEvent evt) {
+	}
+	
+	public void keyTyped(KeyEvent evt) {
+	}
 	
 }
